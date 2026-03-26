714035d7a5e4a048953d0ec2463665c1e11c5d0e
carlyg1234 <carly.glomstad@gmail.com>
2026-03-26 13:54:28 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Simple JavaScript for the button
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('my-button');
+    
+    button.addEventListener('click', function() {
+        alert('Hello! You clicked the button!');
+    });
+});
