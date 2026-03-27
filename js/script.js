937ba4416f8d405c07a5fde674d0f14ea34f6df9
carlyg1234 <carly.glomstad@gmail.com>
2026-03-26 23:07:21 -0500

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Simple JavaScript for the button
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('my-button');
-    
-    button.addEventListener('click', function() {
-        alert('Hello! You clicked the button!');
-    });
-});
